18ab6f8d8576be98659e647aeec243e1c06793e9
needones <needones@163.com>
2019-04-11 22:17:34 +0800

    bug修改，建议使用分开的模块，不容易出错

--- spider/study.py
+++ spider/study.py
@@ -7,6 +7,9 @@ from time import sleep
 from selenium import webdriver
 
 # driver = webdriver.Chrome()
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+
 ops = webdriver.ChromeOptions()
 # ops.add_argument('--headless')
 ops.add_argument('--disable-gpu')
@@ -29,7 +32,6 @@ for i in data:
 sleep(1)
 driver.refresh()
 sleep(3)
-
 
 # 学习理论（阅读文章12分，预计15分钟）
 driver.find_element_by_xpath('//div[@class="father-nav"]/ul[3]/li[1]/a').click()
@@ -56,48 +58,84 @@ for i in list(range(5)):
     print('---------------第%d篇刷完-----------------' % (i + 1))
 driver.close()
 sleep(2)
+
 # 换回首页
 windows = driver.window_handles
 driver.switch_to.window(windows[0])
 
-
-# 学习电视台（观看视频12分，预计30分钟）
+# 学习电视台（观看视频12分，预计40分钟）
 driver.find_element_by_xpath('//div[@class="father-nav"]/ul[2]/li[2]/a').click()
-sleep(1)
+sleep(5)
 windows = driver.window_handles
 driver.switch_to.window(windows[1])
 driver.execute_script("var q=document.documentElement.scrollTop=1000")
 sleep(2)
+
 # 视频区
 # driver.find_element_by_xpath('//div[@id="Chwgg53wi10o00"]/div/div[1]').click()
-driver.find_element_by_xpath('//*[@id="5586"]/div/div/div/div/div/section/div/div/div[1]/div[1]').click()
+try:
+    driver.find_element_by_xpath('//*[@id="5586"]/div/div/div/div/div/section/div/div/div[1]/div[1]').click()
+except:
+    driver.find_element_by_xpath('//div[@id="Chwgg53wi10o00"]/div/div[1]').click()
 sleep(1)
 windows = driver.window_handles
 driver.switch_to.window(windows[2])
 sleep(2)
 list_vedio = []
-for i in list(range(6)):
+for i in list(range(7)):
+    print('第%d次视频' % (i + 1))
     if i < 2:
         pass
     elif i < 4:
-        driver.find_element_by_xpath('//div[@class="radio_2p2eqv4lwtk00"]/div[2]').click()
-        sleep(5)
+        try:
+            driver.find_element_by_xpath('//div[@class="_1KFAyh5wHi8boHp83TMkv-"]/div/div[3]').click()
+            sleep(2)
+        except:
+            try:
+                ActionChains(driver).key_down(Keys.PAGE_UP)
+                sleep(2)
+                driver.find_element_by_xpath('//div[@class="_1KFAyh5wHi8boHp83TMkv-"]/div/div[3]').click()
+                sleep(2)
+            except:
+                try:
+                    driver.find_element_by_xpath('//div[@class="radio_2p2eqv4lwtk00"]/div[2]').click()
+                    sleep(5)
+                except:
+                    pass
     else:
-        driver.find_element_by_xpath('//div[@class="radio_2p2eqv4lwtk00"]/div[3]').click()
-        sleep(5)
+        try:
+            driver.find_element_by_xpath('//div[@class="_1KFAyh5wHi8boHp83TMkv-"]/div/div[4]').click()
+            sleep(2)
+        except:
+            try:
+                ActionChains(driver).key_down(Keys.PAGE_UP)
+                sleep(2)
+                driver.find_element_by_xpath('//div[@class="_1KFAyh5wHi8boHp83TMkv-"]/div/div[4]').click()
+                sleep(2)
+            except:
+                try:
+                    driver.find_element_by_xpath('//div[@class="radio_2p2eqv4lwtk00"]/div[3]').click()
+                    sleep(5)
+                except:
+                    pass
+    # 随机更改视频
     while True:
-        k = random.randint(0, 19)
+        k = random.randint(0, 16)
 
         if k not in list_vedio:
             list_vedio.append(k)
             break
     try:
-        driver.find_elements_by_xpath('//div[@class="_252R0WxMJIuJyNty2pZiaL thePic"]')[k].click()
+        driver.find_elements_by_xpath(
+            '//div[@class="Iuu474S1L6y5p7yalKQbW grid-gr"]//div[@class="_252R0WxMJIuJyNty2pZiaL thePic"]')[k].click()
     except:
         try:
-            driver.find_elements_by_xpath('//div[@class="_3wnLIRcEni99IWb4rSpguK"]/div/div[1]')[k].click()
+            driver.find_elements_by_xpath('//div[@class="_252R0WxMJIuJyNty2pZiaL thePic"]')[k].click()
         except:
-            driver.find_elements_by_xpath('//div[@id="Cd5zymfz1fzs0"]/div/div/div[1]')[k].click()
+            try:
+                driver.find_elements_by_xpath('//div[@id="Cd5zymfz1fzs0"]/div/div/div[1]')[k].click()
+            except:
+                print('第%d次视频获取失败！' % (i + 1))
     sleep(2)
     windows = driver.window_handles
     driver.switch_to.window(windows[3])
@@ -106,17 +144,13 @@ for i in list(range(6)):
     sleep(4)
     # 视频睡眠
     time_s = random.randint(200, 240)
+    # driver.execute_script("var q=document.documentElement.scrollTop=1500")
     sleep(time_s)
     driver.close()
     sleep(2)
     windows = driver.window_handles
     driver.switch_to.window(windows[2])
     sleep(2)
-
-driver.close()
-
-
-
 
 dict_cookie = {}
 a = driver.get_cookies()
